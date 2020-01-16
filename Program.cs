c8fe73323fd739b91dcd3968c0d401453f50c76d
dominik_87 <dominik.swatek@media-press.tv>
2020-01-16 09:04:03 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalExamplesProjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
